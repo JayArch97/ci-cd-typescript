2265bac6d49733685aa474bf168858d1b4ea07a2
Jayarch97 <jayquintanilla2@gmail.com>
2025-07-11 20:54:12 +0000

    Adding Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 //test this 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT || 8081 );
+
 }
-bootstrap();
+bootstrap()
