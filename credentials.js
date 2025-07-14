1cdd579cb678e5d79deadbab09523691cf3d99d5
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-07-14 17:54:57 -0600

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -9,6 +9,7 @@ async function main() {
     scopes: 'https://www.googleapis.com/auth/cloud-platform'
   });
   const client = await auth.getClient();
+  console.log(client)
   const projectId = await auth.getProjectId();
   const url = `https://dns.googleapis.com/dns/v1/projects/${projectId}`;
   const res = await client.request({ url });
