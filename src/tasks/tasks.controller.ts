25096855f916b2ac48ab9cc40fd07ebf81a30301
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-08-14 19:59:40 -0600

    Update tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Tasks } from './tasks.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private readonly tasksService: TasksService) {}
+
+    @Post()
+    async create(
+    @Body('task') task: string,
+    @Body('status') status: string,
+    @Body('follow_up_time') follow_up_time: Date, 
+    @Body('notes') notes: string,
+    @Body('attachment') attachment: boolean,    
+    ) : Promise<Tasks> {
+        return this.tasksService.createTasks( task, status, follow_up_time, notes, attachment);
+    }
+}
