9c59360be803153f3100f33e7c7ea859e8b5bcbc
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-08-14 20:00:09 -0600

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { SequelizeModule } from "@nestjs/sequelize";
 import { TasksController } from './tasks.controller';
+import { Tasks} from './tasks.model'
 
 @Module({
-  providers: [TasksService],
-  controllers: [TasksController]
+    imports: [SequelizeModule.forFeature([Tasks])],
+    providers: [TasksService],
+    controllers: [TasksController]
 })
 export class TasksModule {}
+
